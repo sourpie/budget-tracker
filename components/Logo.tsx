01ea0338a750a78325171a18163c8925aaf2e794
sourpie <111598221+sourpie@users.noreply.github.com>
2025-01-17 20:17:10 +0530

    refactor Logo component to use Next.js Link for navigation

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -1,21 +1,26 @@
-import { PiggyBank } from 'lucide-react'
-import React from 'react'
+import { PiggyBank } from "lucide-react";
+import Link from "next/link";
+import React from "react";
 
 const Logo = () => {
-return (
-    <a href="/" className='flex items-center gap-2'>
-        <PiggyBank className='stroke h-11 w-11 stroke-amber-500 stroke-[1.5]'></PiggyBank>
-        <p className='bg-gradient-to-r from-amber-400 to-orange-500 bg-clip-text text-3xl font-bold leading-tight tracking-tighter text-transparent'>Budget Tracker</p>
-    </a>
-)
-}
+  return (
+    <Link href="/" className="flex items-center gap-2">
+      <PiggyBank className="stroke h-11 w-11 stroke-amber-500 stroke-[1.5]"></PiggyBank>
+      <p className="bg-gradient-to-r from-amber-400 to-orange-500 bg-clip-text text-3xl font-bold leading-tight tracking-tighter text-transparent">
+        Budget Tracker
+      </p>
+    </Link>
+  );
+};
 
 export const LogoMobile = () => {
-    return (
-        <a href="/" className='flex items-center gap-2'>
-            <p className='bg-gradient-to-r from-amber-400 to-orange-500 bg-clip-text text-3xl font-bold leading-tight tracking-tighter text-transparent'>Budget Tracker</p>
-        </a>
-    )
-    }
+  return (
+    <Link href="/" className="flex items-center gap-2">
+      <p className="bg-gradient-to-r from-amber-400 to-orange-500 bg-clip-text text-3xl font-bold leading-tight tracking-tighter text-transparent">
+        Budget Tracker
+      </p>
+    </Link>
+  );
+};
 
 export default Logo;
